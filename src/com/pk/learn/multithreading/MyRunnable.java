6b331b864763e1411e6455fa0ed0e80498985307
Pankaj Nayak <pankajnayak20@gmail.com>
2017-05-31 12:33:35 +0530

    first commit

---
+++ src/com/pk/learn/multithreading/MyRunnable.java
@@ -0,0 +1,28 @@
+package com.pk.learn.multithreading;
+
+/**
+ * MyRunnable will count the sum of the number from 1 to the parameter countUntil and then write the result to the
+ * console.
+ * <p>
+ * MyRunnable is the task which will be performed
+ * 
+ * @author pankaj.nayak
+ * @since May 31, 2017 11:26:51 AM
+ */
+public class MyRunnable implements Runnable {
+
+    private final long countUntil;
+    public MyRunnable(long countUntil) {
+        this.countUntil = countUntil;
+    }
+
+    @Override
+    public void run() {
+        long sum = 0;
+        for (int i = 1; i < countUntil; i++) {
+            sum = sum + i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+}
